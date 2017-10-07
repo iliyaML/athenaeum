6a79aa5009204c7ab559eebff5f10e0bc44dc69b
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-07 10:05:53 -0500

    Implemented google oauth

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// passport config
+require('./config/passport')(passport);
 
 // routes
 const auth = require('./routes/auth');
