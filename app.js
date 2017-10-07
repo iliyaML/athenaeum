c965980c1fb6c35234954be571e7df018dc9f2bd
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-07 10:21:38 -0500

    Connected with MongoDB and created User model

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const passport = require('passport');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const keys = require('./config/keys');
 
 // passport config
 require('./config/passport')(passport);
 
 // routes
 const auth = require('./routes/auth');
+
+// mongodb connection
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
