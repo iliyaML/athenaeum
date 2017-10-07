a460b8fe1c7537adc38c6f65a65e0a54af48482b
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-07 09:48:55 -0500

    Added express router

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// routes
+const auth = require('./routes/auth');
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
