2989a1fc4fd332eb809969759f547d7de6619897
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-07 09:44:27 -0500

    Added config folder

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://iliya:iliya@ds113825.mlab.com:13825/athenaeum',
+  googleClientID: '71806169482-hojs50b5p3r1rcp51a63okd1kb3ohelo.apps.googleusercontent.com',
+  googleClientSecret: 'PuSanB-jDrS43mSVXqioGSsB'
+};
